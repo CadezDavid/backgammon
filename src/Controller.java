e8f0c28ef6ab32880c7a95589bc910cf1d7b09a6
David Čadež <david.cadez7@gmail.com>
2021-07-17 12:00:47 +0200

    made a dir src and rudimentary files

---
+++ src/Controller.java
@@ -0,0 +1,17 @@
+public class Controller {
+    private Model model;
+    private View view;
+
+    public Model getModel() {
+        return model;
+    }
+    public void setModel(Model model) {
+        this.model = model;
+    }
+    public View getView() {
+        return view;
+    }
+    public void setView(View view) {
+        this.view = view;
+    }
+}
