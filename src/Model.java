31a2075a89b28c7a802c554814661266545dac83
David Čadež <david.cadez7@gmail.com>
2021-08-07 10:40:59 +0200

    updated names and merged moveFromBar and move

--- src/Model.java
+++ src/Model.java
@@ -9,138 +9,91 @@ public class Model {
 
     public Model(String nameBP, String nameWP, Type typeBP, Type typeWP) {
 
-        setBoard(new Board());
+        this.board = new Board();
 
-        setBlackPlayer(new Player(nameWP, typeWP));
-        setWhitePlayer(new Player(nameBP, typeBP));
+        this.blackPlayer = new Player(nameWP, typeWP);
+        this.whitePlayer = new Player(nameBP, typeBP);
 
-        setState(State.STARTING);
+        this.state = State.STARTING;
     }
-
 
     public Board getBoard() {
         return board;
-    }
-
-    public void setBoard(Board board) {
-        this.board = board;
     }
 
     public Player getBlackPlayer() {
         return blackPlayer;
     }
 
-    public void setBlackPlayer(Player blackPlayer) {
-        this.blackPlayer = blackPlayer;
-    }
-
     public Player getWhitePlayer() {
         return whitePlayer;
-    }
-
-    public void setWhitePlayer(Player whitePlayer) {
-        this.whitePlayer = whitePlayer;
     }
 
     public State getState() {
         return state;
     }
 
-    public void setState(State state) {
-        this.state = state;
-    }
-
 }
 
 class Board {
 
-    private int[] points;
-
-    private Bar bar;
+    private int[] pips;
 
     public Board() {
         // positive numbers represent black checkers and negative numbers
-        // represent white checkers, with black going right (towards 23) and white
-        // going left (towards 0)
-        points = new int[] { 2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2 };
-
-        bar = new Bar();
+        // represent white checkers, with black going right (towards 24) and white
+        // going left (towards 1)
+        // 0 - black player's bar (value n means n checkers on bar)
+        // 25 - white player's bar (value -n means n checkers on bar)
+        pips = new int[] { 0, 2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2, 0 };
     }
 
     public void move(Checker checker, Move move) {
         int c = (checker == Checker.BLACK ? 1 : -1);
 
         // case in which the opposing player has been hit
-        if (c * points[move.getEndPoint()] == -1) {
-            points[move.getStartPoint()] -= c;
-            points[move.getEndPoint()] = c;
+        if (c * pips[move.getEndPip()] == -1) {
+            pips[move.getStartPip()] -= c;
+            pips[move.getEndPip()] = c;
+            pips[(25 + 25 * c) / 2] -= c;
         }
         // usual case
         else {
-            points[move.getStartPoint()] -= c;
-            points[move.getEndPoint()] += c;
+            pips[move.getStartPip()] -= c;
+            pips[move.getEndPip()] += c;
         }
     }
 
     public boolean isMoveValid(Checker checker, Move move) {
         int c = (checker == Checker.BLACK ? 1 : -1);
 
-        // if checkers on board make sense
-        if ((c * points[move.getStartPoint()] > 0) && (c * points[move.getEndPoint()] > -2)) {
-            return false;
-        }
-
-        // if player has no checkers on the bar
-        if ((checker == Checker.BLACK && bar.getBlack() > 0) || (checker == Checker.WHITE && bar.getWhite() > 0)) {
-            return false;
-        }
-
-        // if the direction of moving is correct
-        if ((move.getEndPoint() - move.getStartPoint()) * c <= 0) {
-            return false;
-        }
-
-        return true;
-    }
-
-    public void moveFromBar(Checker checker, int endPoint) {
-        int c = (checker == Checker.BLACK ? 1 : -1);
-
-        // case in which the opposing player has been hit
-        if (c * points[endPoint] == -1) {
-            if (checker == Checker.BLACK) {
-                bar.removeBlack();
-            } else {
-                bar.removeWhite();
+        // if player is move from the bar
+        if (move.getStartPip() == 0 || move.getStartPip() == 25) {
+            // if checkers on the board make sense
+            if ((pips[move.getStartPip()] > 0) && (c * pips[move.getEndPip()] > -2)) {
+                return false;
             }
-            points[endPoint] = c;
+
+            // if player has no checkers on the bar
+            if (pips[(25 - 25 * c) / 2] == 0) {
+                return false;
+            }
         }
         // usual case
         else {
-            if (checker == Checker.BLACK) {
-                bar.removeBlack();
-            } else {
-                bar.removeWhite();
+            // if checkers on the board make sense
+            if ((c * pips[move.getStartPip()] > 0) && (c * pips[move.getEndPip()] > -2)) {
+                return false;
             }
-            points[endPoint] += c;
-        }
-    }
 
-    public boolean isMoveFromBarValid(Checker checker, int endPoint) {
-        int c = (checker == Checker.BLACK ? 1 : -1);
-
-        // if player has at least one checker on bar
-        if ((checker == Checker.BLACK && bar.getBlack() == 0) || (checker == Checker.WHITE && bar.getWhite() == 0)) {
-            return false;
-        }
-
-        // if checkers on board make sense
-        if (c * points[endPoint] > -2) {
-            return false;
+            // if player has checkers on the bar
+            if (pips[(25 - 25 * c) / 2] != 0) {
+                return false;
+            }
         }
 
         // if the direction of moving is correct
-        if ((endPoint + (23 * c - 23) / 2) * c > 0) {
+        if ((move.getEndPip() - move.getStartPip()) * c <= 0) {
             return false;
         }
 
@@ -148,15 +101,10 @@ class Board {
     }
 
     public boolean canBearOff(Checker checker) {
-        // if player has at least one checker on bar
-        if ((checker == Checker.BLACK && bar.getBlack() > 0) || (checker == Checker.WHITE && bar.getWhite() > 0)) {
-            return false;
-        }
+        int c = (checker == Checker.BLACK ? 0 : 7);
 
-        // if all checkers are in home board
-        int c = (checker == Checker.BLACK ? 0 : 6);
-        for (int i = c; i < 18 + c; i++) {
-            if (points[i] * c > 0) {
+        for (int i = c; i < 19 + c; i++) {
+            if (pips[i] * c > 0) {
                 return false;
             }
         }
@@ -164,63 +112,6 @@ class Board {
         return true;
     }
 
-    public int[] getpoints() {
-        return points;
-    }
-
-    public void setpoints(int[] points) {
-        this.points = points;
-    }
-
-    public int[] getPoints() {
-        return points;
-    }
-
-    public void setPoints(int[] points) {
-        this.points = points;
-    }
-}
-
-class Bar {
-    private int white;
-    private int black;
-
-    public Bar() {
-        setWhite(0);
-        setBlack(0);
-    }
-
-    public void addWhite() {
-        white += 1;
-    }
-
-    public void addBlack() {
-        black += 1;
-    }
-
-    public void removeWhite() {
-        white -= 1;
-    }
-
-    public void removeBlack() {
-        black -= 1;
-    }
-
-    public int getWhite() {
-        return white;
-    }
-
-    public void setWhite(int white) {
-        this.white = white;
-    }
-
-    public int getBlack() {
-        return black;
-    }
-
-    public void setBlack(int black) {
-        this.black = black;
-    }
 }
 
 class Player {
@@ -230,51 +121,35 @@ class Player {
     private Type type;
 
     public Player(String name, Type type) {
-        setName(name);
-        setType(type);
+        this.name = name;
+        this.type = type;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Type getType() {
         return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
     }
 
 }
 
 class Move {
-    private int startPoint;
-    private int endPoint;
+    private int startPip;
+    private int endPip;
 
-    public Move(int startPoint, int endPoint) {
-        setStartPoint(startPoint);
-        setStartPoint(endPoint);
+    public Move(int startPip, int endPip) {
+        this.startPip = startPip;
+        this.endPip = endPip;
     }
 
-    public int getStartPoint() {
-        return startPoint;
+    public int getStartPip() {
+        return startPip;
     }
 
-    public void setStartPoint(int startPoint) {
-        this.startPoint = startPoint;
-    }
-
-    public int getEndPoint() {
-        return endPoint;
-    }
-
-    public void setEndPoint(int endPoint) {
-        this.endPoint = endPoint;
+    public int getEndPip() {
+        return endPip;
     }
 
 }
